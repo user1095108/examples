2fe170155d4bcb445448a6686e94f02291fddf8e
Janez Zemva <janezz55@gmail.com>
2018-11-25 07:13:16 +0100

    some fixes

--- pascal.cpp
+++ pascal.cpp
@@ -2,25 +2,25 @@
 
 #include <iostream>
 
-unsigned pascal1(unsigned i, unsigned N)
+unsigned pascal1(unsigned n, unsigned k)
 {
-  if (!i || (i == N - 1))
+  if (!k || (n == k))
   {
     return 1;
   }
   else
   {
-    return pascal1(i - 1, N - 1) + pascal1(i, N - 1);
+    return pascal1(n - 1, k - 1) + pascal1(n - 1, k);
   }
 }
 
 int main()
 {
-  for (unsigned i{1}; i != 11; ++i)
+  for (unsigned i{}; i != 10; ++i)
   {
-    for (unsigned j{}; j != i; ++j)
+    for (unsigned j{}; j != i + 1; ++j)
     {
-      std::cout << pascal1(j, i) << " ";
+      std::cout << pascal1(i, j) << " ";
     }
 
     std::cout << std::endl;
