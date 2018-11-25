eee0513b61b2cba0dea15959f4861af9059229d7
Janez Zemva <janezz55@gmail.com>
2018-11-25 06:21:08 +0100

    some fixes

--- mystery.cpp
+++ mystery.cpp
@@ -22,25 +22,49 @@ std::string mystery(index_t const i, unsigned n)
   }
 }
 
+std::string mystery2(index_t i, unsigned n)
+{
+  char r[n]{};
+
+  auto size{index_t{1} << n};
+
+  while (n--)
+  {
+    if (i < (size >> 1))
+    {
+      r[sizeof(r) - n - 1] = '0';
+    }
+    else
+    {
+      r[sizeof(r) - n - 1] = '1';
+      i = size - i - 1;
+    }
+
+    size >>= 1;
+  }
+
+  return {r, sizeof(r)};
+}
+
 int main()
 {
-  unsigned n;
+  unsigned n{2};
 
-  std::cin >> n;
+  //std::cin >> n;
 
   auto const size{index_t{1} << n};
 
-/*
   for (index_t i{}; i != size; ++i)
   {
-    std::cout << mystery(i, n) << std::endl;
+    std::cout << mystery2(i, n) << std::endl;
   }
-*/
 
+/*
   for (index_t i{size - n}, end(size); i != end; ++i)
   {
     std::cout << mystery(i, n) << std::endl;
   }
+*/
 
   return 0;
 }
